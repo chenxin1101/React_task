cf483d6bdabcd1822fd116831c488f424603fd23
chenxin1101 <1013450070@qq.com>
2020-10-22 23:34:15 +0800

    1022

---
+++ task3/precache-manifest.c65d2549680984851006e2f523aab351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0869d0ae44cacb8fdd9fee7f7c2d357c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9666698c62aabb496d",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "dc9666698c62aabb496d",
+    "url": "/static/js/2.8f6a0125.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.8f6a0125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8730514e2ddbbc9343",
+    "url": "/static/js/main.9ef48673.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
